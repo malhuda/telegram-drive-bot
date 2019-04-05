97db00c59b4cfb894aa2c29798b6660e9d3c437e
Yash Garg <ben10.yashgarg@gmail.com>
2019-04-06 00:35:13 +0530

    Update utils.js

--- download_tools/utils.js
+++ download_tools/utils.js
@@ -154,14 +154,11 @@ function generateStatusMessage (totalLength, completedLength, speed, files) {
     progress = Math.round(completedLength * 100 / totalLength);
   }
   var totalLengthStr = formatSize(totalLength);
-  var message = 'Downloading: ' + fileName;
-  var prog = 'Progress:' + 'Downloaded' + formatSize(completedLength) + ' / ' + totalLengthStr +
-    '- (' + progress + '%) of <i>' + 'at' + formatSize(speed) + 'ps';
+  var message = 'Downloading : ' + fileName + '\nProgress : Downloaded ' + formatSize(completedLength) + ' / ' + totalLengthStr + ' - ' + progress + '%' + ' at ' + formatSize(speed) + 'ps';
   
   var status = {
     message: message,
     filename: fileName,
-    prog: prog,
     filesize: totalLengthStr
   };
   return status;
